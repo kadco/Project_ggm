c0151c4b5c5c980233ef2ddc71b4b43618f5f282
kadco <kadco69@naver.com>
2020-11-02 01:27:05 +0900

    .

--- GGMUnity2020/Assets/Scripts/AnimationTest.cs
+++ GGMUnity2020/Assets/Scripts/AnimationTest.cs
@@ -6,19 +6,32 @@ public class AnimationTest : MonoBehaviour
 {
     public Animation anima; //편집 //Ctrl+6
 
-    Animator anim;          //Animator, Animator Controller, AnimatorClip
+    public AnimationClip clip;
+    //Animator anim;          //Animator, Animator Controller, AnimationClip
 
     void Start()
     {
         //에니메이션
         print("" + anima.name);
 
+        //anima.wrapMode = WrapMode.Loop;
+
+        //anima.clip = clip; //기본 변경.
     }
 
     void Update()
     {
+        if(Input.anyKeyDown)
+        {
+            anima.Play("CubeAnim2");
+            //anima.PlayQueued("CubeAnim2");
+            //anima.Blend("CubeAnim2");
+            //anima.IsPlaying(anima.name)
+
+        }
     }
 
+    //이벤트 처리 함수
     public void PrintEvent(string s)
     {
         Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
